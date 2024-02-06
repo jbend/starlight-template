770979523ff216229473f1662f34719ea9a0decc
Jeff Bendixsen <jeff_bendixsen@trimble.com>
2024-02-06 06:28:41 -0800

    Updating config

--- templates/trimble-cloud/astro.config.mjs
+++ templates/trimble-cloud/astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
 		starlight({
 			title: 'Trimble Documentation',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/jbend/starlight-template',
 			},
 			customCss: ["starlight-modus"],
 			sidebar: [
